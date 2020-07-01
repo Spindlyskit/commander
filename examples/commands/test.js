75cc6069a6eab0b141bd77c3b524d9f80f28b7f4
Spindlyskit <josh.lavelle1@gmail.com>
2020-07-01 12:29:32 +0100

    Add command loading

---
+++ examples/commands/test.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+'use strict';
+
+const { Command } = require('../..');
+
+/**
+ * Basic command that just echos a test message
+ */
+class TestCommand extends Command {
+	constructor(client) {
+		super(client, 'test', {
+			aliases: ['t'],
+			description: 'Echo a test message',
+		});
+	}
+
+	run(msg) {
+		msg.channel.send(`Hello, ${msg.author}`);
+	}
+}
+
+module.exports = TestCommand;
--- examples/example.js
+++ examples/example.js
@@ -4,10 +4,14 @@
 const config = require('./config.json');
 const commander = require('..');
 
+const TestCommand = require('./commands/test');
+
 const client = new commander.Client({ mentionable: true });
 
 client.on('messageMatched', (msg, prefix) => {
 	console.log(`Message "${msg.content}" (${msg.id}) matched with prefix "${prefix}"`);
 });
+
+client.addCommand(TestCommand);
 
 client.login(config.token);
